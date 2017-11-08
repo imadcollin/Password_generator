f2587388d0c3843971b385968ba3864349449c23
Imad Collin <Admin@MacBook-Pro.local>
2017-11-08 23:16:01 +0100

    Use password generator

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Index file 
-console.log('test')
+
+var generator = require('generate-password');
+
+var password = generator.generate({
+   length: 10,
+   numbers: true
+});
+
+// 'uEyMTw32v9' 
+console.log(password);
