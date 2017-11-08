57862f0f01e707f84d5b110f8ab6c859ec71d8fd
Imad Collin <Admin@MacBook-Pro.local>
2017-11-09 00:04:05 +0100

    Add math calcution for entropy

--- index.js
+++ index.js
@@ -3,28 +3,34 @@
 var generator = require('generate-password');
 var entropy = require('string-entropy');
 
-let arr=[]
-const words=4;
-for(let i=0;i<words;i++){
+let arr = []
+const words = 4;
+const length = 10;
+for (let i = 0; i < words; i++) {
 
-    password=generator.generate({
-        length: 10,
+    password = generator.generate({
+        length,
         numbers: true,
         uppercase: true,
-        symbols:false,
-        uppercase:true
-        
-     });
+        symbols: false,
+        uppercase: true
+
+    });
 
 
-arr.push(password)
-} 
+    arr.push(password)
+}
 
 //print passwords
-let str=''
-for(let i=0;i<arr.length;i++){
-str+=arr[i]+' '
+let str = ''
+for (let i = 0; i < arr.length; i++) {
+    str += arr[i] + ' '
 }
 console.log(str)
-console.log(words+' RANDOM words from word list of NNNNNN words \n')
-console.log( 'Entropy:'+ entropy(str)+' bits');
+console.log(words + ' RANDOM words from word list of NNNNNN words \n')
+console.log('Entropy:' + entropy(str) + ' bits');
+
+
+pow = Math.pow(62, length)
+log = Math.log2(pow)
+console.log('You need ' + log + ' RANDOM characters in [a-zA-Z0-9] to get the same entropy.')
